672cfaecaa8646c72ad65dd3d8a5051055362dd5
Pavlysh <pgfu619@mail.ru>
2024-02-19 16:24:27 +0300

    add New.go

---
+++ New.go
@@ -0,0 +1,17 @@
+// package main
+
+// import "fmt"
+
+// func kekus(x float64) {
+// 	var count float64 = 0.0
+// 	for z := 1.0; z < 11; z++ {
+// 		count = z*z - x
+// 		fmt.Println(count)
+// 	}
+// 	return
+// }
+
+// func main() {
+// 	// fmt.Println(kekus(1))
+// 	kekus(1)
+// }
